658f24a42809dd6ec70a7ad5e7a7428c2239f82e
showdae <showdae3721@naver.com>
2023-03-02 19:01:59 +0900

    230302

--- keras/keras08_train_test3.py
+++ keras/keras08_train_test3.py
@@ -2,50 +2,48 @@
 # 힌트 사이킷런
 # train, test set 분할
 
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from sklearn.model_selection import train_test_split    # 훈련 / 테스트 데이타 섞는 클래스
 
+# 전처리기
+import numpy as np                                      # 넘파이 자료형 (배열간의 연산)
+from tensorflow.keras.models import Sequential          # 모델 Sequential 클래스 사용
+from tensorflow.keras.layers import Dense               # 층 Dense 클래스 사용
+from sklearn.model_selection import train_test_split    # train_test_split 클래스 사용
 
-#1. 데이터 분리 (셔플 적용)
-x = np.array([1,2,3,4,5,6,7,8,9,10])
-y = np.array([1,2,3,4,5,6,7,8,9,10])
+#1. 데이타
+x = np.array([1,2,3,4,5,6,7,8,9,10])                    # x 배열 선언
+y = np.array([1,2,3,4,5,6,7,8,9,10])                    # y 배열 선언
 
-x_train, x_test, y_train, y_test = train_test_split(
+x_train, x_test, y_train, y_test = train_test_split(    # 4개 변수 선언, x,y 데이타 스플릿
     x, y,
-    #train_size = 0.7,            # 훈련 데이타의 비율
-    test_size = 0.3,              # 테스트 데이타의 비율
-    shuffle = True,               # 데이타를 섞는다 (디폴트: True)
-    random_state = 123            # 랜덤 Seed 고정 (파라메타 튜닝을 위함)
-    )
+    # train_size= 0.7,                                  # 훈련 데이타 비율
+    test_size= 0.3,                                     # 평가 데이타 비율
+    shuffle = True,                                     # 셔플 플래그
+    random_state = 123                                  # 랜덤 시프 set
+)
 
-print('X_train:', x_train)
-
-print('X_test:', x_test)
-
-print('y_train:', y_train)
-
-print('y_test:', y_test)
+print('x_train: ', x_train)
+print('x_test: ', x_test)
+print('y_train: ', y_train)
+print('y_test: ', y_test)
 
 #2. 모델
-model = Sequential()
-model.add(Dense(5, input_dim = 1))
-model.add(Dense(7))
+model = Sequential()                                    # 순차적 모델
+model.add(Dense(5, input_dim=1))                        # in layer: input = 1, output = 5
+model.add(Dense(7))                                     # hidden layer
 model.add(Dense(10))
 model.add(Dense(15))
-model.add(Dense(20))
-model.add(Dense(8))
+model.add(Dense(10))
+model.add(Dense(7))
 model.add(Dense(5))
-model.add(Dense(1))
+model.add(Dense(1))                                     # out layer: output = 1
 
 #3. 컴파일/훈련
-model.compile(loss='mse', optimizer = 'adam')
-model.fit(x_train, y_train, epochs=100, batch_size = 1)     # 훈련 데이타를 매개변수로
+model.compile(loss='mse', optimizer = 'adam')           # 컴파일, mse: 절대값
+model.fit(x_train, y_train, epochs=100, batch_size=5)   # 훈련, batch_size: 한번 학습할때 데이타의 수
 
 #4. 평가/예측
-loss = model.evaluate(x_test, y_test)                       # 평가 데이타를 매개변수로
-print('평가: ', loss)
+loss = model.evaluate(x_test, y_test)                   # 훈련되지 않은 데이타의 평가
+print('평가', loss)
 
-result = model.predict([11])                                # y 11번째 요소 예측
-print('예측[11]: ', result)
+result = model.predict([11])                            # 임의의 값 [11] 예측
+print('예측', result)
